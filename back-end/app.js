a615709ec6dffed8720bfb02a853e2751204ca03
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2021-12-01 08:54:10 -0500

    data testing

--- back-end/app.js
+++ back-end/app.js
@@ -3,15 +3,24 @@ const cors = require("cors");
 const express = require("express");
 
 // CONFIGURATION
+
 const app = express();
+const carsController = require("./controllers/carsController");
+
+// MIDDLEWARE
 
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json()); // Parse incoming JSON
 
+
 // ROUTES
+
+
+app.use("/cars", carsController);
+
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Welcome to!");
 });
 
 /////////////////////////////////////
@@ -19,13 +28,16 @@ app.get("/", (req, res) => {
 /////////////////////////////////////
 const db = require("./db/dbConfig.js");
 
-app.get("/test", async (req, res) => {
-  try {
-    const allDays = await db.any("SELECT * FROM test");
-    res.json(allDays);
-  } catch (err) {
-    res.json(err);
-  }
+// app.get("/test", async (req, res) => {
+//   try {
+//     const allDays = await db.any("SELECT * FROM test");
+//     res.json(allDays);
+//   } catch (err) {
+//     res.json(err);
+//   }
+// });
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
 });
 
 /////////////////////////////////////
--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,6 +1,30 @@
-DROP TABLE IF EXISTS test;
+-- DROP TABLE IF EXISTS test;
 
-CREATE TABLE test (
-    id SERIAL PRIMARY KEY, 
-    name TEXT
-);
+-- CREATE TABLE test (
+--     id SERIAL PRIMARY KEY, 
+--     name TEXT
+-- );
+DROP DATABASE IF EXISTS mileage_db;
+
+CREATE DATABASE mileage_db;
+
+\c mileage_db;
+
+CREATE TABLE
+    cars(
+        id SERIAL PRIMARY key,
+        make TEXT not null,
+        model TEXT not null,
+        vin TEXT not null,
+        year INT not null,
+        odometer INT, 
+        doors INT,
+        is_default BOOLEAN NOT NULL,
+        uid TEXT not null,
+        driver TEXT not null
+    );
+
+
+
+
+
