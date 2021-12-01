eac6814a791887622538ebc57deb51c55aae41d7
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2021-12-01 10:38:15 -0500

    test database6

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,25 +1,10 @@
 DROP TABLE IF EXISTS test;
 
 CREATE TABLE test (
-    id SERIAL PRIMARY KEY, 
+    -- id SERIAL PRIMARY KEY, 
+     name TEXT not null,
      osis TEXT not null,
         description TEXT not null,
        number TEXT not null,
         year INT not null,
 );
-
-
-
-CREATE TABLE
-    test(
-        id SERIAL PRIMARY key,
-        make TEXT not null,
-        model TEXT not null,
-        vin TEXT not null,
-        year INT not null,
-        odometer INT, 
-        doors INT,
-        is_default BOOLEAN NOT NULL,
-        uid TEXT not null,
-        driver TEXT not null
-    );
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -8,8 +8,7 @@
 -- ('Sunday');
 
 
-INSERT INTO
-    test(name,osis,description,number,year)
+INSERT INTO test(name,osis,description,number,year)
 VALUES 
     ('Dodge','Charger','1234567890AUPMNTR',7,'2021-01-12'),
     ('Impala','Charger','1234567890AUPMNTR',7,'2021-01-12'),
